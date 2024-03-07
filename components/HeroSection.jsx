1a0c234936dac7c91dbc5c0d0aa46d3f76e66bee
fahim2204 <fahimfaisal1998@gmail.com>
2024-03-08 04:36:16 +0600

    Feat: Implement TTS voice with text highlight

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,35 +1,115 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
 import Link from "next/link";
+import React, { useState, useRef, useEffect } from 'react';
 
 const HeroSection = () => {
+
+
+
 
     const VoiceArtistList = [
         {
             name: 'Ann George',
             voice: 'Male English Voice',
             img: 'artist-1.webp',
-            url: '#',
+            text: 'Welcome to voice hero which can read to you',
         },
         {
             name: 'Jordyn George',
             voice: 'Female English Voice',
             img: 'artist-2.webp',
-            url: '#',
+            text: 'Welcome to voice hero which can read to you',
         },
         {
             name: 'Ann George',
             voice: 'Male English Voice',
             img: 'artist-3.webp',
-            url: '#',
+            text: 'Welcome to voice hero which can read to you',
         },
         {
             name: 'Jordyn George',
             voice: 'Female English Voice',
             img: 'artist-4.webp',
-            url: '#',
+            text: 'Welcome to voice hero which can read to you',
         },
     ]
+
+    const TTSComponent = ({ item, index }) => {
+        const [highlightSection, setHighlightSection] = useState({
+            from: 0,
+            to: 0
+        });
+
+        const [percentage, setPercentage] = useState(0);
+        const [textLength, setTextLength] = useState(0);
+
+        useEffect(() => {
+            setTextLength(item.text.length);
+        }, []);
+
+        const handlePlayTTS = () => {
+            const synth = window.speechSynthesis;
+
+            if (!synth) {
+                console.error("no tts");
+                return;
+            }
+            let isPlaying = false;
+
+            let utterance = new SpeechSynthesisUtterance(item.text);
+            utterance.addEventListener("boundary", (event) => {
+                const { charIndex, charLength } = event;
+                setPercentage(((charIndex + charLength) / textLength) * 100);
+                setHighlightSection({ from: charIndex, to: charIndex + charLength });
+            });
+            utterance.addEventListener('end', () => {
+                setHighlightSection({ from: 0, to: 0 });
+                isPlaying = false;
+            });
+            utterance.addEventListener('cancel', () => {
+                setHighlightSection({ from: 0, to: 0 });
+                isPlaying = false;
+            });
+
+            // synth.speak(utterance);
+            if (isPlaying) {
+                synth.cancel();
+            } else {
+                synth.speak(utterance);
+                isPlaying = true;
+            }
+        };
+
+
+        const [start, highlight, finish] = [
+            item.text.slice(0, highlightSection.from),
+            item.text.slice(highlightSection.from, highlightSection.to),
+            item.text.slice(highlightSection.to)
+        ]
+
+        return (
+            <div key={index} className="rounded-full flex justify-between p-2 md:p-3 items-center bg-beta w-full hover:bg-beta-100 transition-all duration-300">
+                <div className="flex justify-center items-center gap-4">
+                    <Image className="size-14 md:size-16 object-contain" src={`/image/${item.img}`} height={100} width={100} alt={item.name} />
+
+                    {highlightSection.from === 0 && highlightSection.to === 0 ? <>
+                        <div className="flex flex-col md:gap-0.5">
+                            <div className="font-bold md:text-xl">{item.name}</div>
+                            <div className="text-sm md:text-base">{item.voice}</div>
+                        </div>
+                    </> : <>
+                        <p className='font-semibold text-lg'>
+                            {start}
+                            <span className={`bg-alpha text-white rounded ${highlight ? 'px-1' : ''}`}>{highlight}</span>
+                            {finish}
+                        </p>
+                    </>}
+                </div>
+                <button onClick={handlePlayTTS} className=' flex-shrink-0'><Image className="size-14 md:size-16 object-contain" src={'/icon/play.svg'} height={80} width={80} alt='play' /></button>
+            </div>
+        );
+    }
 
     return (
         <div className="max-w-[1400px] mx-auto z-0 relative overflow-hidden">
@@ -47,18 +127,10 @@ const HeroSection = () => {
                 <div className='bg-white p-3 md:p-5 my-16 shadow-sm rounded-3xl grid md:grid-cols-2 gap-4 md:gap-5'>
                     {VoiceArtistList.map((item, index) => {
                         return (
-                            <Link href={item.url} key={index} className="rounded-full flex justify-between p-2 md:p-3 items-center bg-beta w-full hover:bg-beta-100 transition-all duration-300">
-                                <div className="flex justify-center items-center gap-4">
-                                    <Image className="size-14 md:size-16 object-contain" src={`/image/${item.img}`} height={100} width={100} alt={item.name} />
-                                    <div className="flex flex-col md:gap-0.5">
-                                        <div className="font-bold md:text-xl">{item.name}</div>
-                                        <div className="text-sm md:text-base">{item.voice}</div>
-                                    </div>
-                                </div>
-                                <Image className="size-14 md:size-16 object-contain" src={'/icon/play.svg'} height={80} width={80} alt='play' />
-                            </Link>
-                        );
-                    })}
+                            <TTSComponent {...{ item, index }} />
+                        )
+                    }
+                    )}
                 </div>
             </div>
             <div className="container max-w-5xl my-16">
